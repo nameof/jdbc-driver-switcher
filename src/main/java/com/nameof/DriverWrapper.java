14dbd39f7c9eb53ff953afed704f4fdd6116a436
constexpr <constexpr@foxmail.com>
2020-09-23 09:54:03 +0800

    add javadoc

--- src/main/java/com/nameof/DriverWrapper.java
+++ src/main/java/com/nameof/DriverWrapper.java
@@ -5,6 +5,16 @@ import java.sql.*;
 import java.util.Properties;
 import java.util.logging.Logger;
 
+/**
+ * DriverManager存在隔离机制，只允许你访问调用方所属类加载器实例能加载的Driver（即驱动类和调用者类由同一类加载器实例加载），例如普通Java程序使用的System Classloader
+ * 所以，使用URLClassLoader去手动加载Driver类，并不能直接注册到DriverManager使用
+ * 需要DriverWrapper这样一个普通类（这个类由调用者的System Classloader等加载器加载）包装，交给DriverManager使用
+ *
+ * @see java.sql.DriverManager#isDriverAllowed
+ *
+ * https://stackoverflow.com/questions/14478870/dynamically-load-the-jdbc-driver
+ * https://www.kfu.com/~nsayer/Java/dyn-jdbc.html
+ */
 public class DriverWrapper implements Driver {
 
     private Driver driver;
